a969a25c2a698c3f323d663a6b51a7362fddcab3
Hoàng Văn Huy <hoanghuy1162006@gmail.com>
2026-05-13 19:50:11 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ========================================================
-echo DANG KHOI CHAY QUA TRINH DONG GOI SMARTATTEND...
-echo ========================================================
-python build.py
-echo.
-pause
